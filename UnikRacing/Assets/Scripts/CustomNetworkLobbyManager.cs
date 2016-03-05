12568b99d00d8957f0c478a3f53bf50e7925a476
Daniel Hofheinz <hofheinzdaniel@yahoo.de>
2016-03-05 20:28:11 +0100

    i started writing a custom network lobby manager for a small game i made and because our plan is to develop a network game i thought i could include it into my branch at this point it can't do anything else than the one that is included in unity but i'm gonna work on it by time

---
+++ UnikRacing/Assets/Scripts/CustomNetworkLobbyManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Rafiwui.Networking
+{
+    public class CustomNetworkLobbyManager : NetworkLobbyManager
+    {
+        [Space]
+        [Space]
+        [Header("Custom Elements")]
+        public string gameTitle;
+
+        [Space]
+        [Tooltip("Time in seconds until the game starts after every player is ready")]
+        public int prematchCountdown;
+
+        [Space]
+        [Header("UI Elements")]
+        public Text title;
+        public Text countdown;
+        public InputField lANGameName;
+        public InputField lANGamePassword;
+
+        #region Coroutines
+        IEnumerator CountdownToStart()
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        #endregion
+
+        void Start()
+        {
+            title.text = gameTitle;
+        }
+
+        #region OverrideFunctions
+        //TODO:
+        //-start countdown
+        //-change scene
+        public override void OnLobbyServerPlayersReady()
+        {
+            base.OnLobbyServerPlayersReady();
+        }
+        #endregion
+
+        #region OnClickButtonEvents
+        public void OnClickHostButton()
+        {
+            StartMatchMaker();
+            matchMaker.CreateMatch(lANGameName.text, (uint)maxPlayers, true, lANGamePassword.text, OnMatchCreate);
+        }
+
+        public void OnClickSearchButton()
+        {
+
+        }
+
+        public void OnClickLocalServerButton()
+        {
+
+        }
+        #endregion
+
+        #region OnEndEditEvents
+        public void OnEndEditLANHostName(string name)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                OnClickHostButton();
+            }
+        }
+        #endregion
+
+    }
+}
