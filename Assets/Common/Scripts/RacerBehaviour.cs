bba889e7aff2dcb174d55ca4571802fa79c83620
lukas-123 <lukas.steve@web.de>
2016-03-14 20:30:15 +0100

    Improve documentation

--- Assets/Common/Scripts/RacerBehaviour.cs
+++ Assets/Common/Scripts/RacerBehaviour.cs
@@ -106,7 +106,7 @@ public class RacerBehaviour : MonoBehaviour
         {
 
             // Add force to make the vehicle hover over the ground.
-            // http://answers.unity3d.com/questions/46024/maintain-heightaltitudedistance-from-ground.html
+            // Enable upside-down driving by applying force even if the vehicle is above hoverHeight
             if (hit.distance < 2.0f * hoverHeight)
             {
                 rigidBody.AddForce(-transform.up * hoverForce * Mathf.Pow(hit.distance - hoverHeight, 3.0f));
