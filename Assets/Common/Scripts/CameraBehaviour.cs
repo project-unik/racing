d336f2a48a52231ab6f9e1788a483f886e3cb0d9
Felix Fröhlich <felix.froehlich+github@convey.de>
2016-03-30 17:50:32 +0200

    changed CameraBehaviour so that camera smoothing is possible changed constants in Tags from static readonly to const added comments to Utility and UtilityExtensions removed CameraMovement

--- Assets/Common/Scripts/CameraBehaviour.cs
+++ Assets/Common/Scripts/CameraBehaviour.cs
@@ -1,7 +1,7 @@
 ﻿/*!
  * @file Assets/Behaviours/CameraBehaviour.cs
  * @created 2016/02/28
- * @lastmodified 2016/03/26
+ * @lastmodified 2016/03/30
  * @brief Implements the camera behaviour following the player.
  *   Must be placed on the target camera object.
  *
@@ -15,85 +15,144 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using System.Collections;
 
+/// <summary>
+/// Based on https://unity3d.com/learn/tutorials/projects/stealth/camera-movement
+/// </summary>
 [RequireComponent(typeof(Camera))]
 public class CameraBehaviour : MonoBehaviour
 {
     #region camera settings
     /// <summary>
-    /// Initial distance from <see cref="targetObject"/>. This is automatically initialized in <see cref="Start"/>.
+    /// speed at which the camera will catch up to the followed object
     /// </summary>
-    [ReadOnly]
     [SerializeField]
-    private float cameraDistance;
+    [Range(0.0f, 50.0f)]
+    private float smooth = 20f;
 
     /// <summary>
-    /// Initial vertical offset from <see cref="targetObject"/>. This is automatically initialized in <see cref="Start"/>.
+    /// whether or not the camera should take the target object's up/down rotation into account when positioning itself
+    /// </summary>
+    [SerializeField]
+    private bool honourUpDownRotation = true;
+
+    /// <summary>
+    /// position which the camera is trying to reach
     /// </summary>
     [ReadOnly]
     [SerializeField]
-    private float cameraVerticalOffset;
+    private Vector3 newPosition;
 
+    /// <summary>
+    /// horizontal distance of the camera from the followed object
+    /// </summary>
     [ReadOnly]
     [SerializeField]
-    private Vector3 cameraOldTarget;
+    private float horizontalDistance;
 
+    /// <summary>
+    /// vertical distance of the camera from the followed object
+    /// </summary>
+    [ReadOnly]
     [SerializeField]
-    [Range(0, 1)]
-    private float updateLag = 0.5f;
-
-    [SerializeField]
-    [Range(1, 100)]
-    private float cameraSpeed = 2.0f;
-
-    [SerializeField]
-    private bool honourUpDownRotation;
+    private float verticalDistance;
     #endregion
 
-    #region target object components
+    #region targets
     /// <summary>
-    /// The target's <see cref="Rigidbody"/> component.
-    /// </summary>
-    [ReadOnly]
-    [SerializeField]
-    private Rigidbody targetRigidbody;
-
-    /// <summary>
-    /// The target <see cref="GameObject"/> that the camera should follow. Must have a <see cref="Rigidbody"/> component attached.
+    /// reference to the target's transform
     /// </summary>
     [SerializeField]
-    private GameObject targetObject;
+    private Transform target;
     #endregion
 
-    /// <summary>
-    /// <see cref="Transform"/> on the race track which is used for determining the camera's position.
-    /// </summary>
-    public Transform trackObject;
-
-    private void Start()
+    #region Unity messages
+    private void Awake()
     {
-        Assert.IsNotNull(targetObject, "camera is missing a target object");
-        Assert.IsNotNull(targetRigidbody = targetObject.GetComponent<Rigidbody>(), "target object is missing a rigidbody component");
+        // set target transform
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag(Tags.GameObjects.PLAYER).transform;
+        }
+        Assert.IsNotNull(target, "camera is missing target transform");
 
-        Vector3 diff = transform.position - targetRigidbody.position;
-        cameraDistance = Mathf.Sqrt(diff.x * diff.x + diff.z * diff.z);
-        cameraVerticalOffset = diff.y;
-        cameraOldTarget = targetRigidbody.position;
+        // set relative position
+        Vector3 rel = transform.position - target.position;
+        horizontalDistance = Mathf.Sqrt(rel.x * rel.x + rel.z * rel.z);
+        verticalDistance = rel.y;
+        // horizontalDistance = relativePosition.magnitude - 0.5f; // why -0.5f?
     }
 
-    private void Update()
+    #region Update messages
+    private void FixedUpdate()
     {
-        Vector3 xzOffset = -targetRigidbody.transform.forward * cameraDistance;
-
+        // horizontal offset from the target's position
+        Vector3 horizontalOffset = -target.forward * horizontalDistance;
         if (!honourUpDownRotation)
         {
-            xzOffset.y = 0;
+            horizontalOffset.y = 0;
         }
 
-        Vector3 yOffset = targetRigidbody.transform.up * cameraVerticalOffset;
-        Vector3 targetPosition = targetRigidbody.position + xzOffset + yOffset;
-        Vector3 direction = targetPosition - transform.position;
-        Vector3 movement = direction * Time.deltaTime * cameraSpeed;
+        // vertical offset from the target's position
+        Vector3 verticalOffset;
+        if (honourUpDownRotation)
+        {
+            verticalOffset = target.up * verticalDistance;
+        }
+        else
+        {
+            verticalOffset = Vector3.up * verticalDistance;
+        }
 
-        transform.position = transform.position.easeInOut(transform.position + movement, 1 - updateLag);
+        // relative position of the camera from the target object
+        Vector3 standard = target.position + horizontalOffset + verticalOffset;
+
+        // position directly above the target object at the same distance as standard
+        Vector3 above = target.position + Vector3.up * horizontalDistance;
+
+        // points that are used to check if the camera can see the target object
+        Vector3[] checkPoints = new Vector3[5];
+        checkPoints[0] = standard;
+        checkPoints[1] = standard.EaseInOut(above, 0.25f);
+        checkPoints[2] = standard.EaseInOut(above, 0.5f);
+        checkPoints[3] = standard.EaseInOut(above, 0.75f);
+        checkPoints[4] = above;
+
+        // check from which position the camera can see the target object
+        foreach (Vector3 point in checkPoints)
+        {
+            if (CanSeeTargetFrom(point))
+            {
+                newPosition = point;
+                break;
+            }
+        }
+
+        // smooth camera position between its current position and the new position
+        transform.position = transform.position.EaseInOut(newPosition, smooth * Time.deltaTime);
+
+        // look at the target
+        LookAtTarget();
+    }
+    #endregion
+    #endregion
+
+    private bool CanSeeTargetFrom(Vector3 position)
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(position, target.position - position, out hit, horizontalDistance))
+        {
+            if (hit.transform != target) return false;
+        }
+
+        return true;
+    }
+
+    private void LookAtTarget()
+    {
+        Vector3 relativePosition = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePosition, Vector3.up);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, smooth * Time.deltaTime);
     }
 }
--- Assets/Common/Scripts/Util/Utility.cs
+++ Assets/Common/Scripts/Util/Utility.cs
@@ -4,13 +4,35 @@ using System.Collections;
 
 public static class Utility
 {
-    public static float easeInOut(float start, float change, float percentage)
+    /// <summary>
+    /// Eases a <see cref="float"/> value between <code><paramref name="start"/></code> and <code><paramref name="start"/> + <paramref name="change"/></code>.
+    /// Equivalent to <see cref="EaseInOut(float, float, float, float)"/> with <code>time = <paramref name="percentage"/></code> and <code>duration = 1</code>.
+    /// </summary>
+    /// <param name="start">value at <code><paramref name="percentage"/> = 0</code></param>
+    /// <param name="change">difference to <code><paramref name="start"/></code> at <code><paramref name="percentage"/> = 1</code></param>
+    /// <param name="percentage">progress of the ease-in-out method; automatically clamped to [0; 1]</param>
+    /// <returns>
+    /// An eased value between <code><paramref name="start"/></code> and <code><paramref name="start"/> + <paramref name="change"/></code>,
+    /// dependent on <code><paramref name="percentage"/></code>.
+    /// </returns>
+    public static float EaseInOut(float start, float change, float percentage)
     {
-        if (percentage < 0 || percentage > 1) throw new ArgumentException(percentage.ToString(), "percentage");
-        return easeInOut(percentage, start, change, 1);
+        percentage = percentage.Clamp(0, 1);
+        return EaseInOut(percentage, start, change, 1);
     }
 
-    public static float easeInOut(float time, float start, float change, float duration)
+    /// <summary>
+    /// Eases a <see cref="float"/> value between <code><paramref name="start"/></code> and <code><paramref name="start"/> + <paramref name="change"/></code>.
+    /// </summary>
+    /// <param name="time">progress of the ease-in-out method</param>
+    /// <param name="start">value at <code><paramref name="time"/> = 0</code></param>
+    /// <param name="change">difference to <code><paramref name="start"/></code> at <code><paramref name="time"/> = <paramref name="duration"/></code></param>
+    /// <param name="duration">duration of the whole ease-in-out method</param>
+    /// <returns>
+    /// An eased value between <code><paramref name="start"/></code> and <code><paramref name="start"/> + <paramref name="change"/></code>, 
+    /// dependent on the ratio of <code><paramref name="time"/></code> and <code><paramref name="duration"/></code>.
+    /// </returns>
+    public static float EaseInOut(float time, float start, float change, float duration)
     {
         time /= duration / 2;
         if (time < 1) return change / 2 * time * time + start;
@@ -18,30 +40,44 @@ public static class Utility
         return -change / 2 * (time * (time - 2) - 1) + start;
     }
 
-    public static Vector3 easeInOut(Vector3 from, Vector3 to, float percentage)
+    /// <summary>
+    /// Eases a <see cref="Vector3"/> between <code><paramref name="from"/></code> and <code><paramref name="to"/></code>.
+    /// Equivalent to <see cref="EaseInOut(float, Vector3, Vector3, float)"/> with <code>time = <paramref name="percentage"/></code> and <code>duration = 1</code>.
+    /// </summary>
+    /// <param name="from">value at <code><paramref name="percentage"/> = 0</code></param>
+    /// <param name="to">value at <code><paramref name="percentage"/> = 1</code></param>
+    /// <param name="percentage">progress of the ease-in-out method; automatically clamped to [0; 1]</param>
+    /// <returns>
+    /// An eased <see cref="Vector3"/> between <code><paramref name="from"/></code> and <code><paramref name="to"/></code>,
+    /// dependent on <code><paramref name="percentage"/></code>.
+    /// </returns>
+    public static Vector3 EaseInOut(Vector3 from, Vector3 to, float percentage)
     {
-        float xDiff = to.x - from.x;
-        float x = easeInOut(from.x, xDiff, percentage);
-
-        float yDiff = to.y - from.y;
-        float y = easeInOut(from.y, yDiff, percentage);
-
-        float zDiff = to.z - from.z;
-        float z = easeInOut(from.z, zDiff, percentage);
-
-        return new Vector3(x, y, z);
+        percentage = percentage.Clamp(0, 1);
+        return EaseInOut(percentage, from, to, 1);
     }
 
-    public static Vector3 easeInOut(float time, Vector3 from, Vector3 to, float duration)
+    /// <summary>
+    /// Eases a <see cref="Vector3"/> between <code><paramref name="from"/></code> and <code><paramref name="to"/></code>.
+    /// </summary>
+    /// <param name="time">progress of the ease-in-out method</param>
+    /// <param name="from">value at <code><paramref name="time"/> = 0</code></param>
+    /// <param name="to">value at <code><paramref name="time"/> = <paramref name="duration"/></code></param>
+    /// <param name="duration">duration of the whole ease-in-out method</param>
+    /// <returns>
+    /// An eased <see cref="Vector3"/> between <code><paramref name="from"/></code> and <code><paramref name="to"/></code>,
+    /// dependent on the ratio of <code><paramref name="time"/></code> and <code><paramref name="duration"/></code>.
+    /// </returns>
+    public static Vector3 EaseInOut(float time, Vector3 from, Vector3 to, float duration)
     {
         float xDiff = to.x - from.x;
-        float x = easeInOut(time, from.x, xDiff, duration);
+        float x = EaseInOut(time, from.x, xDiff, duration);
 
         float yDiff = to.y - from.y;
-        float y = easeInOut(time, from.y, yDiff, duration);
+        float y = EaseInOut(time, from.y, yDiff, duration);
 
         float zDiff = to.z - from.z;
-        float z = easeInOut(time, from.z, zDiff, duration);
+        float z = EaseInOut(time, from.z, zDiff, duration);
 
         return new Vector3(x, y, z);
     }
@@ -49,23 +85,83 @@ public static class Utility
 
 public static class UtilityExtensions
 {
-    public static float easeInOut(this float start, float change, float percentage)
+    /// <summary>
+    /// Clamps this <see cref="int"/> between <code>minimum</code> and <code>maximum</code>.
+    /// Equivalent to <see cref="Mathf.Clamp(int, int, int)"/> with <code>value = this</code>, <code>min = <paramref name="minimum"/></code> and <code>max = <paramref name="maximum"/></code>.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="minimum">minimum value</param>
+    /// <param name="maximum">maximum value</param>
+    /// <returns>A value clamped to [<paramref name="minimum"/>; <paramref name="maximum"/>].</returns>
+    public static float Clamp(this int value, int minimum, int maximum)
     {
-        return Utility.easeInOut(start, change, percentage);
+        return Mathf.Clamp(value, minimum, maximum);
     }
 
-    public static float easeInOut(this float start, float time, float change, float duration)
+    /// <summary>
+    /// Clamps this <see cref="float"/> between <code>minimum</code> and <code>maximum</code>.
+    /// Equivalent to <see cref="Mathf.Clamp(float, float, float)"/> with <code>value = this</code>, <code>min = <paramref name="minimum"/></code> and <code>max = <paramref name="maximum"/></code>.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="minimum">minimum value</param>
+    /// <param name="maximum">maximum value</param>
+    /// <returns>A value clamped to [<paramref name="minimum"/>; <paramref name="maximum"/>].</returns>
+    public static float Clamp(this float value, float minimum, float maximum)
     {
-        return Utility.easeInOut(time, start, change, duration);
+        return Mathf.Clamp(value, minimum, maximum);
     }
 
-    public static Vector3 easeInOut(this Vector3 from, Vector3 to, float percentage)
+    /// <summary>
+    /// Eases this <see cref="float"/> between <code>this</code> and <code>this + <paramref name="change"/></code>.
+    /// Equivalent to <see cref="Utility.EaseInOut(float, float, float)"/> with <code>start = this</code>, <code>change = <paramref name="change"/></code> and <code>percentage = <paramref name="percentage"/></code>.
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="change">difference to <code>this</code> at <code><paramref name="percentage"/> = 1</code></param>
+    /// <param name="percentage">progress of the ease-in-out method</param>
+    /// <returns>An eased value between <code>this</code> and <code>this + <paramref name="change"/></code>, dependent on <code><paramref name="percentage"/></code>.</returns>
+    public static float EaseInOut(this float start, float change, float percentage)
     {
-        return Utility.easeInOut(from, to, percentage);
+        return Utility.EaseInOut(start, change, percentage);
     }
 
-    public static Vector3 easeInOut(this Vector3 from, float time, Vector3 to, float duration)
+    /// <summary>
+    /// Eases this <see cref="float"/> between <code>this</code> and <code>this + <paramref name="change"/></code>.
+    /// Equivalent to <see cref="Utility.EaseInOut(float, float, float, float)"/> with <code>start = this</code>,
+    /// <code>change = <paramref name="change"/></code>, <code>time = <paramref name="time"/></code> and
+    /// <code>duration = <paramref name="duration"/></code>.
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="time">progress of the ease-in-out method</param>
+    /// <param name="change">difference to <code>this</code> at <code><paramref name="time"/> = <paramref name="duration"/></code></param>
+    /// <param name="duration">duration of the whole ease-in-out method</param>
+    /// <returns>
+    /// An eased value between <code>this</code> and <code>this + <paramref name="change"/></code>, 
+    /// dependent on the ratio of <code><paramref name="time"/></code> and <code><paramref name="duration"/></code>.
+    /// </returns>
+    public static float EaseInOut(this float start, float time, float change, float duration)
     {
-        return Utility.easeInOut(time, from, to, duration);
+        return Utility.EaseInOut(time, start, change, duration);
+    }
+
+    /// <summary>
+    /// Eases this <see cref="Vector3"/> between <code>this</code> and <code><paramref name="to"/></code>.
+    /// Equivalent to <see cref="Utility.EaseInOut(Vector3, Vector3, float)"/> with <code>from = this</code>,
+    /// <code>to = <paramref name="to"/></code> and <code>percentage = <paramref name="percentage"/></code>.
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to">value at <code><paramref name="percentage"/> = 1</code></param>
+    /// <param name="percentage">progress of the ease-in-out method; automatically clamped to [0; 1]</param>
+    /// <returns>
+    /// An eased <see cref="Vector3"/> between <code>this</code> and <code><paramref name="to"/></code>,
+    /// dependent on <code><paramref name="percentage"/></code>.
+    /// </returns>
+    public static Vector3 EaseInOut(this Vector3 from, Vector3 to, float percentage)
+    {
+        return Utility.EaseInOut(from, to, percentage);
+    }
+
+    public static Vector3 EaseInOut(this Vector3 from, float time, Vector3 to, float duration)
+    {
+        return Utility.EaseInOut(time, from, to, duration);
     }
 }
