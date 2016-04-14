288e45c5e30a5aa3f8017a040af37127e947f6cd
Lukas Stevens <mail@lukas-stevens.me>
2016-04-14 14:00:33 +0200

    Create seperate server scene

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -5,7 +5,7 @@ public class BuildScript
 
     public static void PerformBuild()
     {
-        string[] scenes = {"Assets/Scenes/Menu.unity"};
+        string[] scenes = {"Assets/Scenes/Server.unity"};
         BuildPipeline.BuildPlayer(scenes, "Build/server.out", BuildTarget.StandaloneLinux, BuildOptions.EnableHeadlessMode);
     }
 }
